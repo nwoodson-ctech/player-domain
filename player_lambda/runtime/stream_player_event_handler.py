623cadd16b0f73ce81e6c63da2fda7995b59dcff
Nathan Woodson <nwoodson@mbp-nwoodson01.fios-router.home>
2022-12-21 13:17:35 -0500

    Encoding event_bus_detail_data

--- player_lambda/runtime/stream_player_event_handler.py
+++ player_lambda/runtime/stream_player_event_handler.py
@@ -14,11 +14,15 @@ logger = Logger(service="StreamPlayerEventHandler")
 @event_source(data_class=EventBridgeEvent)
 @logger.inject_lambda_context
 def handler(event: EventBridgeEvent, context: LambdaContext) -> str:
-    event_bus_detail_data = event.detail
+    event_bus_detail_data = str(event.detail)
     logger.info(event_bus_detail_data)
+
+    event_bus_detail_data_encoded = base64.b64encode(event_bus_detail_data)
+    logger.info(event_bus_detail_data_encoded)
+
     client.put_record(
         StreamName="player-domain-stream",
-        Data=base64.encode(event_bus_detail_data),
+        Data=event_bus_detail_data_encoded,
         PartitionKey="SavePlayer"
     )
 
